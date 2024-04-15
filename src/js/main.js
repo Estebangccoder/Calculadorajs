e8aad6fff292bf37ba553d7deed7fcbe4796e564
estebangccoder <estebangccoder@gmail.com>
2024-04-15 13:12:18 -0500

    Primeros pasos

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+let number1=Number (prompt("Please enter a number one"))
+let number2=Number (prompt("Please enter a number two"))
+
+let userName = prompt("Enter your username:")
+
+const addition=number1+number2
+const substration=number1-number2
+const multiplication=number1*number2
+const division=number1/number2
+
+console.log("La suma es " ,addition, substration, multiplication)
