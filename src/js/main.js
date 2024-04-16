e1d53f613726a4412baa1f85c82d2ccb32060561
estebangccoder <estebangccoder@gmail.com>
2024-04-16 06:46:37 -0500

    Se finaliza el programa

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 let number1=Number (prompt("Please enter a number one"))
 let number2=Number (prompt("Please enter a number two"))
 
-let userName = prompt("Enter your username:")
-
 const addition=number1+number2
 const substration=number1-number2
 const multiplication=number1*number2
 const division=number1/number2
 
-console.log("La suma es " ,addition, substration, multiplication)
+console.log("The addition is " ,addition)
+console.info("The substraction is " ,substration)
+console.error("The multiplication is " ,multiplication)
+console.warn("The division is " ,division)
+
+
